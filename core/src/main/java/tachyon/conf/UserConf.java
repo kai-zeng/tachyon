d3774200463f3b69578aaa755582cbd88828b094
Carson Wang <carson.wang@intel.com>
2015-03-12 10:50:31 +0800

    Set user remote read buffer size to 8MB for better performance

--- core/src/main/java/tachyon/conf/UserConf.java
+++ core/src/main/java/tachyon/conf/UserConf.java
@@ -57,7 +57,7 @@ public class UserConf extends Utils {
     DEFAULT_BLOCK_SIZE_BYTE = getLongProperty("tachyon.user.default.block.size.byte", Constants.GB);
     PAGE_SIZE_BYTE = getLongProperty("tachyon.user.page.size.byte", Constants.MB);
     REMOTE_READ_BUFFER_SIZE_BYTE =
-        getIntProperty("tachyon.user.remote.read.buffer.size.byte", Constants.MB);
+        getIntProperty("tachyon.user.remote.read.buffer.size.byte", 8 * Constants.MB);
     DEFAULT_WRITE_TYPE =
         getEnumProperty("tachyon.user.file.writetype.default", WriteType.CACHE_THROUGH);
   }
